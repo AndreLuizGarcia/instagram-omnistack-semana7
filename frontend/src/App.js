8bced002658bf701bfa03063a1be1037ed6f994b
Andre Garcia <Andre.Garcia@la.logicalis.com>
2019-06-17 13:12:26 -0300

    Default files from react project

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/Header'
+import Routes from './routes'
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes />
+    </BrowserRouter>
+  );
+}
+
+export default App;
